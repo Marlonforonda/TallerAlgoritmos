c0d599badd0f332019922bf3b8d57582b24665e3
Marlon <Marlon@DESKTOP-SJTT2DQ>
2023-09-01 18:02:07 +0200

    Algoritmo punto 1 taller

---
+++ Punto1TallerAlgoritmos/Program.cs
@@ -0,0 +1,51 @@
+﻿//1ro definir clase con su respectivo main
+public class MiPrimerPuntoTaller
+{
+    static void Main(string[] args)
+    {   // Ingresar por pantalla las coordenadas del punto 
+
+        //2do declaración de mis variables
+
+        double x1, x2, y1, y2, distancia;
+
+
+        //3ro pedir los valores por consola
+        Console.Write("Ingrese x1: ");
+        x1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese y1 :");
+        x2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese x2 :");
+        y1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Ingrese y2: ");
+        y2 = Convert.ToDouble(Console.ReadLine());
+
+        //4to Cálculos (procesos)
+        distancia = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+
+        //5to imprimir los resultados (datos de salida)
+        Console.Write("la distancia entre los dos puntos es: " + distancia );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
